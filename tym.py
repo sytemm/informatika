2b2b13de8c593c2358cd051abae024138d0400fd
Muhammad Syuhada Pratama <145412536+sytemm@users.noreply.github.com>
2024-08-18 22:37:34 +0700

    Add files via upload

---
+++ tym.py
@@ -0,0 +1,24 @@
+Statement adalah sebuah instruksi atau kalimat perintah yang akan dieksekusi oleh
+komputer. Contoh:
+print("Hello World!")
+print("Belajar Python dari Nol")
+nama = "python pemula"
+print("Hello"); print("World"); print("Tutorial Python untuk Pemula")
+muhammad_syuhada= "python"
+; pratama = "pemula"
+"Belajar Pemrograman Python sampai Bisa"penulis = 'python pemula'
+judul = """Belajar Python dengan Cepat"""penulis = '''python pemula'''
+Sintak Python bersifat case sensitive, artinya teksini dengan TeksIni dibedakan.
+Contoh:
+judul = "Belajar Dasa-dasar Python"
+Judul = "Belajar Membuat Program Python"
+Contoh yang benar
+if username == 'pythonpemula'
+:
+print("Selamat Datang Admin")
+print("Silakan ambil tempat duduk")
+contoh yang salah
+if username == 'petanikode'
+:
+print("Selamat Datang Admin")
+print("Silakan ambil tempat duduk")
